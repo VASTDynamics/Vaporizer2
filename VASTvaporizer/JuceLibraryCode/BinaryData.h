2c2c768614ab76015d9851e434a269606e98e20c
VastTobias <tobias@vast-dynamics.com>
2023-09-26 17:49:20 +0200

    Initial commit

--- VASTvaporizer/JuceLibraryCode/BinaryData.h
+++ VASTvaporizer/JuceLibraryCode/BinaryData.h
@@ -33,31 +33,31 @@ namespace BinaryData
     const int            Trade_Gothic_LT_ttfSize = 77384;
 
     extern const char*   logokreis_svg;
-    const int            logokreis_svgSize = 3721;
+    const int            logokreis_svgSize = 3802;
 
     extern const char*   logokreisvdalpha_png;
     const int            logokreisvdalpha_pngSize = 31472;
 
     extern const char*   fft_md;
-    const int            fft_mdSize = 17351;
+    const int            fft_mdSize = 17685;
 
     extern const char*   CMakeLists_txt;
-    const int            CMakeLists_txtSize = 3857;
+    const int            CMakeLists_txtSize = 3946;
 
     extern const char*   COPYING;
-    const int            COPYINGSize = 1089;
+    const int            COPYINGSize = 1109;
 
     extern const char*   COPYING_GPLv2;
-    const int            COPYING_GPLv2Size = 18092;
+    const int            COPYING_GPLv2Size = 18431;
 
     extern const char*   Doxyfile;
-    const int            DoxyfileSize = 101673;
+    const int            DoxyfileSize = 104004;
 
     extern const char*   mufft_pc_in;
-    const int            mufft_pc_inSize = 290;
+    const int            mufft_pc_inSize = 302;
 
     extern const char*   README_md;
-    const int            README_mdSize = 5009;
+    const int            README_mdSize = 5121;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
     const int namedResourceListSize = 17;
