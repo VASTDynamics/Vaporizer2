6bc572ed70b1110796e68127bdb93810d833a32f
VastTobias <tobias@vast-dynamics.com>
2023-10-15 15:28:43 +0200

    Cmake

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 cd vastVaporizer
+
 cmake -Bbuild -DCMAKE_PREFIX_PATH=../JUCE/install
-cmake --build build --config Release 
+cmake --build build --config Release -DASIO_SDK_LOCATION="path\to\Vaporizer2\asiosdk" -DAAX_SDK_LOCATION="path\to\aaxsdk"
+
 cd ..
