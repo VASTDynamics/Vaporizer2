d6447aa5e0a77e871e5ad8a9ab65be65bdf12be6
VastTobias <tobias@vast-dynamics.com>
2023-10-15 16:25:50 +0200

    Added Versioning per VERSION file to CMake
    Removed JUCE splash screen

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-cd vastVaporizer
+CLS
+set /p plugin_version=<VERSION
+cmake -Bcmake-build -DCMAKE_PREFIX_PATH=../JUCE/install -DPLUGIN_VERSION:STRING="%plugin_version%" -DASIO_SDK_LOCATION:STRING=".\asiosdk"
+cmake --build cmake-build --config Release 
 
-cmake -Bbuild -DCMAKE_PREFIX_PATH=../JUCE/install
-cmake --build build --config Release -DASIO_SDK_LOCATION="path\to\Vaporizer2\asiosdk" -DAAX_SDK_LOCATION="path\to\aaxsdk"
-
-cd ..
