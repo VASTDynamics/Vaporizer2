f3e7c8f72225f75fc409d6d7cbf0935342bfe095
VAST Dynamics <contact@vast-dynamics.com>
2023-10-19 19:14:32 +0200

    Adapting to macOS Cmake

---
+++ build_macos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set /p plugin_version=<VERSION
+cmake -Bcmake-build -DCMAKE_PREFIX_PATH=../JUCE/install -DPLUGIN_VERSION:STRING="%plugin_version%" -DASIO_SDK_LOCATION:STRING=".\asiosdk"
+cmake --build cmake-build --config Release 
