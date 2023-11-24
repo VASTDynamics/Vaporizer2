fbe34952b42070ca2968469b7e15a951a816a020
VAST Dynamics <contact@vast-dynamics.com>
2023-11-24 09:19:50 +0100

    FFTW build macos

---
+++ fftw-3.3.10/build_fftwlib.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo rm -rf cmake-build
+cmake -B"cmake-build/x64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DBUILD_SHARED_LIBS=0 -DENABLE_FLOAT=1 -DENABLE_SSE=0 -DENABLE_SSE2=0 -DENABLE_AVX=0 -DENABLE_AVX2=0
+cmake --build "cmake-build/x64" --config Release
+
+cmake -B"cmake-build/x64SSE" -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DBUILD_SHARED_LIBS=0 -DENABLE_FLOAT=1 -DENABLE_SSE=0 -DENABLE_SSE2=0 -DENABLE_AVX=0 -DENABLE_AVX2=0
+cmake --build "cmake-build/x64SSE" --config Release
+
+mkdir -p lib
+cp -frv cmake-build/x64/libfftw3f.a lib/libfftw3_x64.a
+cp -frv cmake-build/x64SSE/libfftw3f.a lib/libfftw3_universal.a 
